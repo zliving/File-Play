a70dde471d11ef87e07c9df2d39f0b4a97aae619
zliving <zachary.n.livingston@gmail.com>
2016-10-06 12:35:11 -0500

    Test Fonts

--- core/src/com/mygdx/game/FilePlayMain.java
+++ core/src/com/mygdx/game/FilePlayMain.java
@@ -5,32 +5,34 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.GlyphLayout;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 
 public class FilePlayMain extends ApplicationAdapter {
 	SpriteBatch batch;
 	private BitmapFont font;
+	private GlyphLayout font2;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		font = new BitmapFont();
+		font2 = new GlyphLayout();
 		font.setColor(Color.TEAL);
-		font.
 	}
 
 	@Override
 	public void render () {
 		float width = Gdx.graphics.getWidth();
 		float height = Gdx.graphics.getHeight();
-		float middleScreenWidth = width/2;
+		float middleScreenWidth = (width/2);
 		float middleScreenHeight = height/2;
 
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		font.draw(batch,"File Play!",middleScreenWidth,middleScreenHeight);
+		//font.draw(batch,"File Play!",middleScreenWidth,middleScreenHeight);
 		batch.end();
 
 	}
--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import com.mygdx.game.FilePlayMain;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "File Play";
+		config.width = 480;
+		config.height = 320;
 		new LwjglApplication(new FilePlayMain(), config);
 	}
 }
