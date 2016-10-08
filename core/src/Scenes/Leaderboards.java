b3284a886ab87e2ba1cad7a0eb4c02d58f73342b
Chris.V <cvasqu09@gmail.com>
2016-10-08 15:29:16 -0500

    Added other screens and renamed the settings screen

---
+++ core/src/Scenes/Leaderboards.java
@@ -0,0 +1,44 @@
+package Scenes;
+
+import com.badlogic.gdx.Screen;
+
+/**
+ * Created by Chris on 10/8/2016.
+ */
+
+public class Leaderboards implements Screen{
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
