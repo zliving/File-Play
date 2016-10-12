06aa629612651b09b5abb6c9f95876886a63a9b5
jmtaber129 <jmtaber129@gmail.com>
2016-10-12 00:28:00 -0500

    Added basic test.

---
+++ core/src/test/java/main/java/Scenes/TestSceneTest.java
@@ -0,0 +1,15 @@
+package main.java.Scenes;
+
+import org.junit.Test;
+
+/**
+ * A test for the Test Scene class.
+ * TODO(jmtaber129): Delete this test when the Test class is removed.
+ */
+public class TestSceneTest {
+    @Test
+    public void show() throws Exception {
+        assert(true);
+    }
+
+}
