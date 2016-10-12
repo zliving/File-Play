064f25537943b8475d32eb7557a062877b3c2428
zliving <zachary.n.livingston@gmail.com>
2016-10-11 22:04:57 -0500

    error checking fix and array list

--- core/src/GameObjects/TriviaGame.java
+++ core/src/GameObjects/TriviaGame.java
@@ -11,6 +11,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.lang.reflect.Type;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -21,6 +22,8 @@ import java.util.Map;
 
 public class TriviaGame {
 
+    Gson gson;
+
     public TriviaGame() {
 
     }
@@ -28,31 +31,32 @@ public class TriviaGame {
     public Map getTrivia() {
 
         try {
-            URL url = new URL("http://www.opentdb.com/api.php?amount=10");
+            // Build the URL for OpenDB API query.
+            URL url = new URL("http://www.opentdb.com/api.php?amount=2");
             String as = "";
+
             BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream(), "UTF-8"));
+
             for (String line; (line = reader.readLine()) != null; ) {
                 as += line;
             }
             //as = StringEscapeUtils.unescapeHtml4(as);
-            Gson gson = new Gson();
-            Map<String,Object> map = new HashMap<String,Object>();
-            map = (Map<String,Object>) gson.fromJson(as, map.getClass());
+            gson = new Gson();
 
-            for (Map.Entry<String, Object> entry : map.entrySet()) {
-                String key = entry.getKey();
-                Object value = entry.getValue();
-                System.out.println("key = "+key+" value= "+value+"\n");
-            }
+            Map<String,Object> triviaMap = new HashMap<String,Object>();
+            triviaMap = (Map<String,Object>) gson.fromJson(as, triviaMap.getClass());
+            ArrayList resultsArray = new ArrayList(((ArrayList) triviaMap.get("results")));
+            System.out.println(resultsArray);
 
-            return map;
+            return triviaMap;
 
         } catch (Exception e) {
             Map <String, String> errorMap = new HashMap<String, String>();
-            errorMap.put("Error","No connection could be made to trivia database.");
-            
+            errorMap.put("Error", e.toString());
+
             return errorMap;
         }
+
     }
 
 }
