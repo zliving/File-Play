db74647fac3232a6b224989dd3ecc6fb5b2c3e0b
zliving <zachary.n.livingston@gmail.com>
2016-10-13 14:34:52 -0500

    Add DisplayTriviaQuestions scene

---
+++ core/src/main/java/Scenes/DisplayTriviaQuestions.java
@@ -0,0 +1,44 @@
+package Scenes;
+
+import com.badlogic.gdx.Screen;
+
+/**
+ * Created by zach on 10/13/16.
+ */
+
+public class DisplayTriviaQuestions implements Screen {
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
