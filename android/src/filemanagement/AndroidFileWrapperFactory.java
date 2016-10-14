6baa497eb8db0fd1480e614366aa902211e1f583
jmtaber129 <jmtaber129@gmail.com>
2016-10-14 00:14:48 -0500

    Android implementation of FileWrapperFactory.
    Created the Android implementation of the FileWrapperFactory interface.  Moved Android implementations of FileWrapper and FileWrapperFactory to the Android module to resolve Android dependencies.

---
+++ android/src/filemanagement/AndroidFileWrapperFactory.java
@@ -0,0 +1,20 @@
+package filemanagement;
+
+import java.io.File;
+
+/**
+ * Implementation of the FileWrapperFactory interface for Android.
+ */
+
+public class AndroidFileWrapperFactory implements FileWrapperFactory {
+  @Override
+  public AndroidFileWrapper getGalleryFile() {
+    String galleryPathname = android.os.Environment.DIRECTORY_DCIM;
+    return createFile(galleryPathname);
+  }
+
+  public AndroidFileWrapper createFile(String pathname) {
+    File wrappedFile = new File(pathname);
+    return new AndroidFileWrapper(wrappedFile);
+  }
+}
