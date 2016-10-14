898667ea69e9a723d3007360e1e8dbd155efd31a
jmtaber129 <jmtaber129@gmail.com>
2016-10-13 23:55:19 -0500

    Switched to new indention style.

--- core/src/main/java/filemanagement/FileWrapper.java
+++ core/src/main/java/filemanagement/FileWrapper.java
@@ -4,27 +4,27 @@ package filemanagement;
  * Interface for a wrapper of the File class.
  */
 public interface FileWrapper {
-    /*
-     *  Returns a string describing the file path.
-     */
-    public String getFilePath();
+  /*
+   *  Returns a string describing the file path.
+   */
+  public String getFilePath();
 
-    /*
-     *  Returns a list of files contained in the directory described by 'this'.
-     *  Returns an empty list if 'this' is an empty directory or a file.
-     */
-    public FileWrapper[] getFileList();
+  /*
+   *  Returns a list of files contained in the directory described by 'this'.
+   *  Returns an empty list if 'this' is an empty directory or a file.
+   */
+  public FileWrapper[] getFileList();
 
-    /*
-     *  Returns true if 'this' is moved to 'destination' successfully, returns false otherwise.
-     *  Usage:
-     *  FileWrapper src;
-     *  FileWrapper dst;
-     *
-     *  // Initialize src and dst.
-     *
-     *  // Move src to dst.
-     *  src.Move(dst.getFilePath());
-     */
-    public Boolean Move(FileWrapper destination);
+  /*
+   *  Returns true if 'this' is moved to 'destination' successfully, returns false otherwise.
+   *  Usage:
+   *  FileWrapper src;
+   *  FileWrapper dst;
+   *
+   *  // Initialize src and dst.
+   *
+   *  // Move src to dst.
+   *  src.Move(dst.getFilePath());
+   */
+  public Boolean Move(FileWrapper destination);
 }
