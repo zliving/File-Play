b6ae7a1192bd077c47da507998925c06cc4646c1
zliving <zachary.n.livingston@gmail.com>
2016-11-08 14:05:11 -0600

    Update URL connection

--- core/src/main/java/GameObjects/TriviaQuestionBuilder.java
+++ core/src/main/java/GameObjects/TriviaQuestionBuilder.java
@@ -9,8 +9,12 @@ import org.apache.commons.lang3.StringEscapeUtils;
 
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.MalformedURLException;
 import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
 
 
 /**
@@ -22,52 +26,96 @@ public class TriviaQuestionBuilder {
   }
 
   public Array<TriviaQuestion> getTriviaQuestions(String triviaApiUrl) {
+
     try {
-      // Build the URL for OpenTDB API query using BufferedReader to get the url contents.
-      URL url = new URL(triviaApiUrl);
-      String html = "";
-      BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream(), "UTF-8"));
-      for (String line; (line = reader.readLine()) != null; ) {
-        html += line;
-      }
-      // Close the buffered reader connection.
-      reader.close();
-      // Take the JSON retrieved from OpenTDB API and parse it into a JSON object.
-      JsonValue root = new JsonReader().parse(html);
-      JsonValue resultJson = root.get("results");
-      Array<TriviaQuestion> questions = new Array<TriviaQuestion>();
-      if (root.get("response_code").asInt() != 0) {
+      String questionJSON = "";
+      String line;
+      URL url;
+      url = new URL(triviaApiUrl);
+      HttpsURLConnection con = (HttpsURLConnection)url.openConnection();
+
+      // If we have an open connection get the trivia from OpenTDB
+      if(con!=null){
+
+        try {
+
+          BufferedReader br =
+                  new BufferedReader(
+                          new InputStreamReader(con.getInputStream()));
+          // Read through the OpenTDB response page.
+          while ((line = br.readLine()) != null){
+            questionJSON += line;
+          }
+          br.close();
+
+          // Take the JSON retrieved from OpenTDB API and parse it into a JSON object.
+          JsonValue root = new JsonReader().parse(questionJSON);
+          JsonValue resultJson = root.get("results");
+          Array<TriviaQuestion> questions = new Array<TriviaQuestion>();
+
+          if (root.get("response_code").asInt() != 0) {
+            Array<TriviaQuestion> errorArray = new Array<TriviaQuestion>();
+            TriviaQuestion errorQuestion = new TriviaQuestion();
+            errorQuestion.errorMessage = "API Could not be reached";
+            errorArray.add(errorQuestion);
+
+            return errorArray;
+          }
+
+          // Iterate through the resulting JSON and put each question into a TriviaQuestion object.
+          for (JsonValue resultsJson : resultJson.iterator()) {
+            TriviaQuestion newQuestion = new TriviaQuestion();
+            newQuestion.question = StringEscapeUtils.unescapeHtml4(resultsJson.getString("question"));
+            newQuestion.correctAnswer = (resultsJson.getString("correct_answer"));
+            newQuestion.incorrectAnswers = (resultsJson.get("incorrect_answers").asStringArray());
+            questions.add(newQuestion);
+          }
+
+          // Iterate through each incorrect answer and unescape the HTML that might be in them.
+          for (int i = 0; i < questions.size; i++) {
+            for (int j = 0; j < questions.get(i).incorrectAnswers.length; j++) {
+              questions.get(i).incorrectAnswers[j] =
+                      StringEscapeUtils.unescapeHtml4(questions.get(i).incorrectAnswers[j]);
+            }
+          }
+
+          return questions;
+
+        }
+        catch (IOException e) {
+          e.printStackTrace();
+        }
+
+      } else {
+        // There is no connection to the OpenTDB.
+        // Send an error back to user if questions can't be retrieved.
         Array<TriviaQuestion> errorArray = new Array<TriviaQuestion>();
         TriviaQuestion errorQuestion = new TriviaQuestion();
-        errorQuestion.errorMessage = "API Could not be reached";
+        errorQuestion.errorMessage = "No connection can be made.";
         errorArray.add(errorQuestion);
+        
         return errorArray;
       }
-      // Iterate through the resulting JSON and put each question into a TriviaQuestion object.
-      for (JsonValue resultsJson : resultJson.iterator()) {
-        TriviaQuestion newQuestion = new TriviaQuestion();
-        newQuestion.question = StringEscapeUtils.unescapeHtml4(resultsJson.getString("question"));
-        newQuestion.correctAnswer = (resultsJson.getString("correct_answer"));
-        newQuestion.incorrectAnswers = (resultsJson.get("incorrect_answers").asStringArray());
-        questions.add(newQuestion);
-      }
-      // Iterate through each incorrect answer and unescape the HTML that might be in them.
-      for (int i = 0; i < questions.size; i++) {
-        for (int j = 0; j < questions.get(i).incorrectAnswers.length; j++) {
-          questions.get(i).incorrectAnswers[j] =
-              StringEscapeUtils.unescapeHtml4(questions.get(i).incorrectAnswers[j]);
-        }
-      }
-      return questions;
 
-    } catch (Exception e) {
+    } catch (MalformedURLException e) {
       // Send an error back to user if questions can't be retrieved.
       Array<TriviaQuestion> errorArray = new Array<TriviaQuestion>();
       TriviaQuestion errorQuestion = new TriviaQuestion();
-      errorQuestion.errorMessage = e.toString();
+      errorQuestion.errorMessage = "URL is not valid. " + e.toString();
       errorArray.add(errorQuestion);
+
+      return errorArray;
+    } catch (IOException e) {
+      // Send an error back to user if questions can't be retrieved.
+      Array<TriviaQuestion> errorArray = new Array<TriviaQuestion>();
+      TriviaQuestion errorQuestion = new TriviaQuestion();
+      errorQuestion.errorMessage = "IO Exception? " + e.toString();
+      errorArray.add(errorQuestion);
+
       return errorArray;
     }
+
+    return null;
   }
 
 }
