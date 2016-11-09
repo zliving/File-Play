74e944b5c8b16ee40d7cdd3750ac24630c1c3ec4
zliving <zachary.n.livingston@gmail.com>
2016-11-09 14:01:19 -0600

    Space formatting

--- core/src/main/java/InputListeners/TriviaButtonListener.java
+++ core/src/main/java/InputListeners/TriviaButtonListener.java
@@ -5,22 +5,21 @@ import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 
 /**
- * Created by zach on 10/24/16.
- * TriviaButtonListener is a gesture listener specifically for trivia buttons.
- * When each button is clicked the listener will make calculations on each button.
+ * Created by zach on 10/24/16. TriviaButtonListener is a gesture listener specifically for trivia
+ * buttons. When each button is clicked the listener will make calculations on each button.
  */
 
 public class TriviaButtonListener extends InputListener {
 
-    // Check if there is an initial button pressed.
-    public boolean touchDown (InputEvent event, float x, float y, int pointer, int button) {
-        Gdx.app.log("Button has been ", "Pressed");
-        return true;
-    }
+  // Check if there is an initial button pressed.
+  public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+    Gdx.app.log("Button has been ", "Pressed");
+    return true;
+  }
 
-    // When button is released calculations will be performed here.
-    public void touchUp (InputEvent event, float x, float y, int pointer, int button) {
-        Gdx.app.log("Button ahs been", "Released");
-    }
+  // When button is released calculations will be performed here.
+  public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
+    Gdx.app.log("Button ahs been", "Released");
+  }
 
 }
