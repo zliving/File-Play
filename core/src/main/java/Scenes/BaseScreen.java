fff77e2ef279810c160a261f3e8752494f3f7759
zliving <zachary.n.livingston@gmail.com>
2016-11-14 14:55:30 -0600

    Update background to dark gray

--- core/src/main/java/Scenes/BaseScreen.java
+++ core/src/main/java/Scenes/BaseScreen.java
@@ -81,7 +81,7 @@ public abstract class BaseScreen implements Screen {
    */
   @Override
   public void render(float delta) {
-    Gdx.gl.glClearColor(0, 0, 0, 1);
+    Gdx.gl.glClearColor(86/255f, 86/255f, 86/255f, 1);
     Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
     spriteBatch.setProjectionMatrix(camera.combined);
     spriteBatch.begin();
