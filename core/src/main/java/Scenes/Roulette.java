5b33a8248eb9aa19cd3ccd624a4d9c3cc3bd7a6d
Chris.V <cvasqu09@gmail.com>
2016-11-23 19:24:52 -0600

    Copied code for the ring animation.

--- core/src/main/java/Scenes/Roulette.java
+++ core/src/main/java/Scenes/Roulette.java
@@ -1,6 +1,9 @@
 package Scenes;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.GlyphLayout;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.mygdx.game.FilePlayMain;
 
 /**
@@ -8,20 +11,35 @@ import com.mygdx.game.FilePlayMain;
  */
 
 public class Roulette extends BaseScreen{
+  private TextureAtlas ringsAtlas;
+  private Animation animation;
+  private float elapsedTime = 0.0f;
+
   public Roulette(FilePlayMain mainGame){
     super(mainGame);
     bannerTextGlyphLayout = new GlyphLayout(bannerText, "Roulette");
-
+    // Generate a new animation for the roulette wheel and import the ring atlas.
+    ringsAtlas = new TextureAtlas(Gdx.files.internal("ring spritesheet.atlas"));
+    animation = new Animation(1/15f, ringsAtlas.getRegions());
   }
 
   @Override
   public void render(float delta) {
     super.render(delta);
+    stage.act(delta);
+    stage.draw();
+    // Keeps count of the time for the animation to use.
+    elapsedTime += delta;
+    spriteBatch.begin();
+    // Draws and cycles each sprite in the atlas at the center-ish of the screen.
+    spriteBatch.draw(animation.getKeyFrame(elapsedTime, true), 140, WORLD_HEIGHT / 2);
+    spriteBatch.end();
   }
 
   @Override
   public void dispose() {
     super.dispose();
+    ringsAtlas.dispose();
   }
 
   @Override
